ba475e935d035708f6d5d60ecc09b5e3b1edc3a3
Chris-Dee <cmd45@duke.edu>
2014-03-29 15:01:40 -0400

    adds 'levels'

--- src/FrontEnd/ObjectPanel.java
+++ src/FrontEnd/ObjectPanel.java
@@ -3,8 +3,10 @@ package FrontEnd;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class ObjectPanel extends JPanel {
@@ -18,14 +20,19 @@ private void makeMainFrame(){
 }
 private void makeLevelButton(JPanel panel){
 	// TODO add properties file
+	JPanel buttonModule=new JPanel();
+	buttonModule.setLayout(new BoxLayout(buttonModule,BoxLayout.Y_AXIS));
+	final JTextField levelName=new JTextField(0);
+	buttonModule.add(levelName);
 	JButton level=new JButton("Add Level");
 	level.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e)
 		{   
-			levels.addLevel("LEVEL!!!");
+			levels.addLevel(levelName.getText());
 		}
 	});   
-	panel.add(level);
+	buttonModule.add(level);
+	panel.add(buttonModule);
 }
 
 }
