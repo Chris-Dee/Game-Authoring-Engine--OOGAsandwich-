2cddaaaa42cc04fe5acf0db9b251a2e493c88f4b
sam-ginsburg <sbg11@duke.edu>
2014-04-03 02:08:09 -0400

    initial framework attempt for GameEngine / GamePlayer

---
+++ src/gameEngine/GameObject.java
@@ -0,0 +1,16 @@
+package gameEngine;
+
+import jgame.*;
+import jgame.JGObject;
+
+public class GameObject extends JGObject {
+	
+	public GameObject(String name, int x, int y, int colid, String sprite) {
+		super(name,true,x,y,colid,sprite);
+	}
+	
+	public void move() {
+		
+	}
+
+}
