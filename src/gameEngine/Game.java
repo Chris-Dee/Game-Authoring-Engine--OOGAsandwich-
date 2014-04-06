f6012b2015cb68c1ee20ad901b2ba1cf549e7c96
Michael Lai <themichaellai@gmail.com>
2014-04-05 20:09:30 -0400

    Revert "Use reflection for UninstantiatedGameObject#initialize"
    This reverts commit e0f80ff4fac1f0c6ec295eeda3251349f5f5c435.

--- src/gameEngine/Game.java
+++ src/gameEngine/Game.java
@@ -1,7 +1,6 @@
 package gameEngine;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -49,26 +48,12 @@ public class Game {
 		
 		List<UninstantiatedGameObject> objs = new ArrayList<UninstantiatedGameObject>();
 
-		try {
-			objs.add(new UninstantiatedGameObject("GameObject", "test", new JGPoint(10, 10), 1, "hero-r", new Movement("pace",50, 8)));
-			objs.add(new UninstantiatedGameObject("GameObject", "test", new JGPoint(100, 100), 1, "hero-r", new Movement("pace",25, 5)));
-			objs.add(new UninstantiatedGameObject("GameObject", "test", new JGPoint(20, 105), 1, "hero-r", new Movement(4,1)));
-			objs.add(new UninstantiatedGameObject("GameObject", "land", new JGPoint(20, 155), 1, "mytile", new Movement()));
-			objs.add(new UninstantiatedGameObject("GameObject", "land", new JGPoint(25, 135), 1, "mytile", new Movement("pace",75, 2)));
-			objs.add(new UninstantiatedGameObject("GameObject", "land", new JGPoint(30, 125), 1, "mytile", new Movement()));
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+		objs.add(new UninstantiatedGameObject("test", new JGPoint(10, 10), 1, "hero-r", new Movement("pace",50, 8)));
+		objs.add(new UninstantiatedGameObject("test", new JGPoint(100, 100), 1, "hero-r", new Movement("pace",25, 5)));
+		objs.add(new UninstantiatedGameObject("test", new JGPoint(20, 105), 1, "hero-r", new Movement(4,1)));
+		objs.add(new UninstantiatedGameObject("land", new JGPoint(20, 155), 1, "mytile", new Movement()));
+		objs.add(new UninstantiatedGameObject("land", new JGPoint(25, 135), 1, "mytile", new Movement("pace",75, 2)));
+		objs.add(new UninstantiatedGameObject("land", new JGPoint(30, 125), 1, "mytile", new Movement()));
 
 		// This code will eventually be used to parse the data.
 		
--- src/gameEngine/UninstantiatedGameObject.java
+++ src/gameEngine/UninstantiatedGameObject.java
@@ -1,57 +1,22 @@
 package gameEngine;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import jgame.JGPoint;
 
 public class UninstantiatedGameObject {
-	Object[] objArgs;
-	Constructor<?> constructor;
-
-	public UninstantiatedGameObject(String className, Object... args)
-			throws ClassNotFoundException, InstantiationException,
-			IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, NoSuchMethodException {
-		objArgs = args;
-		Class<?> objKlass = Class.forName("gameEngine." + className);
-
-		Constructor<?>[] objConstructors = objKlass.getConstructors();
-		for (Constructor<?> c : objConstructors) {
-			if (constructorFits(c, objArgs)) {
-				constructor = c;
-			}
-		}
-		if (constructor == null) {
-			// Thrown if objects in args don't match constructor in class
-			// corresponding to className
-			throw new NoSuchMethodException();
-		}
+	String objectName;
+	JGPoint objectPosition;
+	int objectColid;
+	String objectSprite;
+	Movement objectMovement;
+	
+	public UninstantiatedGameObject(String name, JGPoint position, int colid, String sprite, Movement move) {
+		objectName = name;
+		objectPosition = position;
+		objectColid = colid;
+		objectSprite = sprite;
+		objectMovement = move;
 	}
-
-	public GameObject instantiate() {
-		try {
-			return (GameObject) constructor.newInstance(objArgs);
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private boolean constructorFits(Constructor<?> constructor, Object[] args) {
-		Class<?>[] paramTypes = constructor.getParameterTypes();
-		if (args.length != paramTypes.length) {
-			return false;
-		}
-		for (int i = 0; i < paramTypes.length; i++) {
-			if (!paramTypes[i].isAssignableFrom(args[i].getClass())) {
-				return false;
-			}
-		}
-		return true;
+	public GameObject instantiate(){
+		return new GameObject(objectName, objectPosition, objectColid, objectSprite, objectMovement);
 	}
 }
