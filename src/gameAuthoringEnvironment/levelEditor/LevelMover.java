d680e463cfc853aae57c6fd929839a1043b053a2
raswaminathan <rahul.swaminathan@duke.edu>
2014-04-06 15:57:49 -0400

    small refactoring deleted unnecessary code

--- src/gameAuthoringEnvironment/levelEditor/LevelMover.java
+++ src/gameAuthoringEnvironment/levelEditor/LevelMover.java
@@ -9,15 +9,15 @@ public class LevelMover extends JGObject {
 	private static final double INITIAL_X_Y_SPEED = 2.0;
 	private static final int BALL_COL_ID = 4;
 	private static final int INITIAL_X_AND_Y = 20;
-	JGEngine myEngine;
-	LevelEditor myLevelEditor;
+	private JGEngine myEngine;
+	private LevelEditor myLevelEditor;
 	private final static String RESOURCE_PATH = "/gameAuthoringEnvironment/levelEditor/Resources/";
 
 	/**
 	 * The object that moves around in the levels so user can edit them
 	 * 
 	 * @param levelEditor
-	 * 				LevelEditor that this object exists in
+	 *            LevelEditor that this object exists in
 	 */
 	public LevelMover(LevelEditor levelEditor) {
 
@@ -29,27 +29,9 @@ public class LevelMover extends JGObject {
 		y = INITIAL_X_AND_Y;
 	}
 
-	public LevelMover(LevelEditor level, int xPos, int yPos) {
-		super("srball",true, xPos, yPos,BALL_COL_ID,"srball",0,0,INITIAL_X_Y_SPEED,INITIAL_X_Y_SPEED,-1);
-		myEngine = level;
-		myLevelEditor = level;
-		x = xPos;
-		y = yPos;
-	}
-	
-	public Integer xPos() {
-
-		return (int) x;
-	}
-
-	public Integer yPos() {
-		return (int) y;
-	}
-
 	private boolean checkKey(int key) {
 		Boolean b = myEngine.getKey(key);
 		myEngine.clearLastKey();
-
 		return b;
 	}
 
@@ -66,11 +48,8 @@ public class LevelMover extends JGObject {
 			ydir = 1;
 	}
 
-	
 	public void changeImage(String imageName) {
-		myLevelEditor.defineImage("srball","n",0,RESOURCE_PATH + imageName,"-");
-		//myLevelEditor.myMover.destroy();
-		//myLevelEditor.myMover = new LevelMover(myLevelEditor, (int) x, (int) y);
+		myLevelEditor.defineImage("srball", "n", 0, RESOURCE_PATH + imageName,
+				"-");
 	}
 }
-
