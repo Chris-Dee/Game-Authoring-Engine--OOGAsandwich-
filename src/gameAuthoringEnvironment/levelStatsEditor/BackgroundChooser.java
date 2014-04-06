a19251966004785d8df7a2e07971fb1d35ab9f4e
Brett Fox <bsf7@duke.edu>
2014-04-06 00:29:14 -0400

    More refactoring

--- src/gameAuthoringEnvironment/levelStatsEditor/BackgroundChooser.java
+++ src/gameAuthoringEnvironment/levelStatsEditor/BackgroundChooser.java
@@ -22,118 +22,141 @@ import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-//ComboBox to choose backgrounds
-//Backgrounds and names are stored in  LevelPanelComponents
-//Can update this slightly when level objects are introduced
-//High level--uses Map to store names to show up in combo box vs. filepaths
-//uses one static method to update the state of the ComboBox dynamically.
 
 //Adding new images is having problems because JGame is being a little bitch.
 
 public class BackgroundChooser extends JPanel {
-	LevelPanel levels;
-	private final String delimiter=" ";
-	private final String initialSelection="White";
-	private final String defaultFile="src/gameAuthoringEnvironment/levelStatsEditor/Resources/InitBG";
-	Map<String,String> backgroundMap=new TreeMap<String,String>();
+	LevelPanel myLevelPanel;
+	private final String delimiter = " ";
+	private final String initialSelection = "White";
+	private final String defaultFile = "src/gameAuthoringEnvironment/levelStatsEditor/Resources/InitBG";
+	Map<String, String> myBackgroundMap = new TreeMap<String, String>();
 	private String uploadedFile;
 	private JTextField bgName;
-	//TODO we may want to not amek this static. Then again...there will only ever be one
+	// TODO we may want to not amek this static. Then again...there will only
+	// ever be one
 	private static JComboBox model;
-	public BackgroundChooser(LevelPanel l) throws FileNotFoundException{
+
+	/**
+	 * ComboBox to choose backgrounds. Backgrounds and names are stored in
+	 * LevelPanelComponents. Can update this slightly when level objects are
+	 * introduced. On a high level, it uses Map to store names to show up in
+	 * combo box vs. filepaths. It uses one static method to update the state of
+	 * the ComboBox dynamically.
+	 * 
+	 * @param levelPanel
+	 * 				LevelPanel object that the new background is assigned to
+	 * @throws FileNotFoundException
+	 */
+	public BackgroundChooser(LevelPanel levelPanel) throws FileNotFoundException {
 		super();
-		levels=l;
-		backgroundMap=initializeImageMap(new File(defaultFile));
+		myLevelPanel = levelPanel;
+		myBackgroundMap = initializeImageMap(new File(defaultFile));
 		makeBackgroundChooser(this);
 		makeFileChooserButton(this);
-		}
-	private Map<String,String> initializeImageMap(File file) throws FileNotFoundException{
-		Map<String,String> bgMap=new TreeMap<String, String>();
-		Scanner s=new Scanner(file);
-		while(s.hasNext()){
-			String[] str=s.nextLine().split(delimiter);
-			bgMap.put(str[0],str[1]);
+	}
+
+	private Map<String, String> initializeImageMap(File file)
+			throws FileNotFoundException {
+		Map<String, String> bgMap = new TreeMap<String, String>();
+		Scanner s = new Scanner(file);
+		while (s.hasNext()) {
+			String[] str = s.nextLine().split(delimiter);
+			bgMap.put(str[0], str[1]);
 		}
 		return bgMap;
 	}
-	public void makeBackgroundChooser(JPanel homePanel){
-		homePanel.setLayout(new BoxLayout(homePanel,BoxLayout.Y_AXIS));
-		model=new JComboBox(backgroundMap.keySet().toArray());
+
+	public void makeBackgroundChooser(JPanel homePanel) {
+		homePanel.setLayout(new BoxLayout(homePanel, BoxLayout.Y_AXIS));
+		model = new JComboBox(myBackgroundMap.keySet().toArray());
 		setSelectionToDefault();
 		model.addItemListener(new ItemChangeListener());
 		homePanel.add(new JLabel("Select Background Image"));
 		homePanel.add(model);
 		homePanel.add(PanelFactory.makeVerticalSpacerPanel(15));
-		//TODO disable model with nothing selected to avoid errors.
-		//model.setEnabled(false);
+		// TODO disable model with nothing selected to avoid errors.
+		// model.setEnabled(false);
 	}
-	public void enableModel(){
+
+	public void enableModel() {
 		model.setEnabled(true);
 	}
-	public void setSelectionToDefault(){
+
+	public void setSelectionToDefault() {
 		model.setSelectedItem(initialSelection);
 	}
-	public void makeFileChooserButton(JPanel homePanel){
-		JPanel editorPanel=new JPanel();
+
+	public void makeFileChooserButton(JPanel homePanel) {
+		JPanel editorPanel = new JPanel();
 		homePanel.add(new JLabel("Add New Background Image"));
-		editorPanel.setLayout(new BoxLayout(editorPanel,BoxLayout.X_AXIS));
+		editorPanel.setLayout(new BoxLayout(editorPanel, BoxLayout.X_AXIS));
 		editorPanel.add(new JLabel("BG name"));
-		bgName=new JTextField(0);
+		bgName = new JTextField(0);
 		editorPanel.add(bgName);
 		homePanel.add(editorPanel);
-		JButton uploadImage=new JButton("upload");
+		JButton uploadImage = new JButton("upload");
 		uploadImage.addActionListener(new FileChoose(this));
 		editorPanel.add(uploadImage);
-		JButton addImage=new JButton("Add Image");
+		JButton addImage = new JButton("Add Image");
 		addImage.addActionListener(new AddImage(this));
 		homePanel.add(addImage);
 	}
-	public void setBackgroundToSelected(){
-		String newBG=backgroundMap.get(model.getSelectedItem());
-		System.out.println("l89BC "+levels);
-		levels.findActivePanel().changeDefaultBackground(newBG, (String)model.getSelectedItem());
+
+	public void setBackgroundToSelected() {
+		String newBG = myBackgroundMap.get(model.getSelectedItem());
+		System.out.println("l89BC " + myLevelPanel);
+		myLevelPanel.findActivePanel().changeDefaultBackground(newBG,
+				(String) model.getSelectedItem());
 	}
-	public static void setSelectedToBackground(String backgroundName){
+
+	public static void setSelectedToBackground(String backgroundName) {
 		model.setSelectedItem(backgroundName);
 	}
+
 	public class FileChoose implements ActionListener {
 		JPanel home;
 
-		public FileChoose(JPanel homePanel){
-			home=homePanel;
+		public FileChoose(JPanel homePanel) {
+			home = homePanel;
 		}
+
 		public void actionPerformed(ActionEvent e) {
-			JFileChooser fileChooser=new JFileChooser();
+			JFileChooser fileChooser = new JFileChooser();
 			int returnVal = fileChooser.showOpenDialog(home);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				String file = fileChooser.getSelectedFile().getAbsolutePath();
-				uploadedFile=file;
+				uploadedFile = file;
 				try {
-					// return the file path 
+					// return the file path
 				} catch (Exception ex) {
-					//TODO deal with exception
+					// TODO deal with exception
 				}
-			}  
+			}
 		}
 	}
-	public class AddImage implements ActionListener{
+
+	public class AddImage implements ActionListener {
 		JPanel home;
-		public AddImage(JPanel homePanel){
-			home=homePanel;
+
+		public AddImage(JPanel homePanel) {
+			home = homePanel;
 		}
+
 		public void actionPerformed(ActionEvent e) {
-			backgroundMap.put(bgName.getText(),uploadedFile);
-			System.out.println(backgroundMap);
+			myBackgroundMap.put(bgName.getText(), uploadedFile);
+			System.out.println(myBackgroundMap);
 			model.addItem(bgName.getText());
 		}
 
 	}
-	class ItemChangeListener implements ItemListener{
-	    public void itemStateChanged(ItemEvent event) {
-	       if (event.getStateChange() == ItemEvent.SELECTED) {
-	          setBackgroundToSelected();
-	          // do something with object
-	       }
-	    }       
+
+	class ItemChangeListener implements ItemListener {
+		public void itemStateChanged(ItemEvent event) {
+			if (event.getStateChange() == ItemEvent.SELECTED) {
+				setBackgroundToSelected();
+				// do something with object
+			}
+		}
 	}
 }
--- src/gameAuthoringEnvironment/levelStatsEditor/BasicLevelStats.java
+++ src/gameAuthoringEnvironment/levelStatsEditor/BasicLevelStats.java
@@ -12,44 +12,52 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+
 public class BasicLevelStats extends JPanel {
 	LevelPanel allLevels;
 	JLabel levelName;
 	JPanel editPane;
-	Font headingFont=new Font("Arial",Font.BOLD,18);
-	public BasicLevelStats(){
+	Font headingFont = new Font("Arial", Font.BOLD, 18);
+
+	/**
+	 * Central component of the GAE. Holds information about the current level,
+	 * ability to add more, etc.
+	 */
+	public BasicLevelStats() {
 		super();
 		makeLevelEditor(this);
 	}
-	public void setLevelPanel(LevelPanel l){
-		allLevels=l;
+
+	public void setLevelPanel(LevelPanel l) {
+		allLevels = l;
 		try {
 			editPane.add(new BackgroundChooser(allLevels));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-public void makeLevelEditor(JPanel mainPanel) {
-	 editPane=new JPanel();
-	mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
-	editPane.setLayout(new BoxLayout(editPane,BoxLayout.Y_AXIS));
-	levelName=new JLabel("Add and Select a Level!");
-	levelName.setFont(headingFont);
-	levelName.setHorizontalAlignment(SwingConstants.CENTER);
-	editPane.add(levelName);
-	editPane.add(PanelFactory.makeVerticalSpacerPanel(30));
-	
-	mainPanel.add(editPane);
-	//mainPanel.add(new LevelSizeSliders());
-	mainPanel.add(PanelFactory.makeVerticalSpacerPanel(800));
-}
-public void setLevelName(String name) {
-	levelName.setText(name);
-	
-	if (name == null){
-		levelName.setText("Please select a level");
-	}
-}
 
+	public void makeLevelEditor(JPanel mainPanel) {
+		editPane = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		editPane.setLayout(new BoxLayout(editPane, BoxLayout.Y_AXIS));
+		levelName = new JLabel("Add and Select a Level!");
+		levelName.setFont(headingFont);
+		levelName.setHorizontalAlignment(SwingConstants.CENTER);
+		editPane.add(levelName);
+		editPane.add(PanelFactory.makeVerticalSpacerPanel(30));
+
+		mainPanel.add(editPane);
+		// mainPanel.add(new LevelSizeSliders());
+		mainPanel.add(PanelFactory.makeVerticalSpacerPanel(800));
+	}
+
+	public void setLevelName(String name) {
+		levelName.setText(name);
+
+		if (name == null) {
+			levelName.setText("Please select a level");
+		}
+	}
 
 }
