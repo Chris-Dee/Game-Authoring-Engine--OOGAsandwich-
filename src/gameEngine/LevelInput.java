d7322a3f3b0f4697686a18cfa6f8bae6f75c928e
Ethan Gottlieb <ethaniragottlieb@gmail.com>
2014-04-06 01:36:50 -0400

    tiny changes

--- src/gameEngine/LevelInput.java
+++ src/gameEngine/LevelInput.java
@@ -3,11 +3,12 @@ package gameEngine;
 import java.util.*;
 
 public class LevelInput {
-	private Map<Character, String[]> characterMap;
-	public LevelInput(Map<Character, String[]> charMap) {
+	private Map<Character, String> characterMap;
+	public LevelInput(Map<Character, String> charMap) {
 		characterMap=charMap;
+		//This class will deal with whole level actions like pausing and viewing main menu
 	}
-	public Map<Character, String[]> getCharMap(){
+	public Map<Character, String> getCharMap(){
 		return characterMap;
 	}
 
