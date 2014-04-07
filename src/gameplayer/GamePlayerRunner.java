360fde8c4048b09aa5e9d643fb0dc587f1808efa
Jeff Hou <j.king.hou@gmail.com>
2014-04-07 17:28:57 -0400

    Changed collisions so objects no longer bounce.

--- src/gameplayer/GamePlayerRunner.java
+++ src/gameplayer/GamePlayerRunner.java
@@ -1,16 +1,9 @@
 package gameplayer;
 
 import java.io.IOException;
-
 import data.InvalidDataFileException;
-import jgame.JGPoint;
 
 public class GamePlayerRunner {
-	/*
-	 * Eventually the following statistics should be defined by 
-	 * Game Author.
-	 */
-	
 	public static void main(String[]args) throws IOException, InvalidDataFileException {
 		new GamePlayerGUI(new MarioGame());
 	}
