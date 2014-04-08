2c4d57ab0dd9ca55345d6f360fc6766b12ec608d
raswaminathan <rahul.swaminathan@duke.edu>
2014-04-08 00:24:03 -0400

    small refactoring

--- src/gameAuthoringEnvironment/levelEditor/LevelEditor.java
+++ src/gameAuthoringEnvironment/levelEditor/LevelEditor.java
@@ -95,46 +95,33 @@ public class LevelEditor extends JGEngine {
 	 *            y position of image
 	 */
 	public void addObject(String imageName, int x, int y) {
+		UninstantiatedGameObject newObject;
 		if (imageName.equals("mario")) {
 			Map<Integer, String> levelInputMap = new HashMap<Integer, String>();
 			levelInputMap.put(39, "moveRight");
 			levelInputMap.put(37, "moveLeft");
 			levelInputMap.put(40, "moveDown");
 			levelInputMap.put(38, "moveUp");
-			UninstantiatedGameObject newObject = new UninstantiatedGameObject(
-					"player", new JGPoint(x, y), 1, imageName, levelInputMap,
-					false);
-			myLevel.addObjects(newObject);
-			newObject.instantiate();
+			newObject = new UninstantiatedGameObject("player",
+					new JGPoint(x, y), 1, imageName, levelInputMap, false);
 		} else if (imageName.equals("block")) {
-			UninstantiatedGameObject newObject = new UninstantiatedGameObject(
-					"block", new JGPoint(x, y), 2, imageName, true);
-			myLevel.addObjects(newObject);
-			newObject.instantiate();
+			newObject = new UninstantiatedGameObject("block",
+					new JGPoint(x, y), 2, imageName, true);
 		} else if (imageName.equals("goomba")) {
-			UninstantiatedGameObject newObject = new UninstantiatedGameObject(
-					"goomba", new JGPoint(x, y), 4, imageName, "pace", 55, 2,
-					false);
-			myLevel.addObjects(newObject);
-			newObject.instantiate();
+			newObject = new UninstantiatedGameObject("goomba",
+					new JGPoint(x, y), 4, imageName, "pace", 55, 2, false);
 		} else if (imageName.equals("pacMan")) {
-			UninstantiatedGameObject newObject = new UninstantiatedGameObject(
-					"stationary platform", new JGPoint(x, y), 2, imageName,
-					true);
-			myLevel.addObjects(newObject);
-			newObject.instantiate();
+			newObject = new UninstantiatedGameObject("stationary platform",
+					new JGPoint(x, y), 2, imageName, true);
 		} else if (imageName.equals("tree")) {
-			UninstantiatedGameObject newObject = new UninstantiatedGameObject(
-					"goal", new JGPoint(x, y), 8, imageName, true);
-			myLevel.addObjects(newObject);
-			newObject.instantiate();
+			newObject = new UninstantiatedGameObject("goal", new JGPoint(x, y),
+					8, imageName, true);
+		} else {
+			newObject = new UninstantiatedGameObject("block",
+					new JGPoint(x, y), 2, imageName, true);
 		}
-		else {
-			UninstantiatedGameObject newObject = new UninstantiatedGameObject(
-					"block", new JGPoint(x, y), 2, imageName, true);
-			myLevel.addObjects(newObject);
-			newObject.instantiate();
-		}
+		myLevel.addObjects(newObject);
+		newObject.instantiate();
 	}
 
 	private void checkInBounds() {
