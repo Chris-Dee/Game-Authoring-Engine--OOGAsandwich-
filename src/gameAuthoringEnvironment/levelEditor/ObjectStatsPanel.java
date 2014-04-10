c97fd049dc8fbcc425b2d6b51e4924833c43139f
raswaminathan <rahul.swaminathan@duke.edu>
2014-04-10 02:11:05 -0400

    started sliders at 0

--- src/gameAuthoringEnvironment/levelEditor/ObjectStatsPanel.java
+++ src/gameAuthoringEnvironment/levelEditor/ObjectStatsPanel.java
@@ -16,7 +16,6 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-
 @SuppressWarnings("serial")
 public class ObjectStatsPanel extends JPanel {
 	private Dimension panelSize;
@@ -24,20 +23,19 @@ public class ObjectStatsPanel extends JPanel {
 	private static final int PANEL_WIDTH = 250;
 	private static final Dimension SPACER_SIZE = new Dimension(PANEL_WIDTH, 50);
 	private static final Dimension COMBO_SIZE = new Dimension(PANEL_WIDTH, 30);
-	
+
 	private String objectName = "Player";
 	private String movementName = "Pace";
-	
-	private Integer movementSpeed = 5;
-	private int movementDuration = 5;
-	
+
+	private Integer movementSpeed = 0;
+	private int movementDuration = 0;
+
 	private int gravityMag = 0;
 	private LevelEditor myEditor;
-	
-	
+
 	/**
-	 * Panel that will display the stats for the object that is being 
-	 * added to the level in the level editor environment
+	 * Panel that will display the stats for the object that is being added to
+	 * the level in the level editor environment
 	 */
 	public ObjectStatsPanel(ObjectEditorContainer container, LevelEditor editor) {
 		panelSize = new Dimension(PANEL_WIDTH, container.HEIGHT);
@@ -46,52 +44,58 @@ public class ObjectStatsPanel extends JPanel {
 		myEditor = editor;
 		myEditor.setObjectStatsPanel(this);
 	}
-	
+
 	private void initialize() {
 		this.setPreferredSize(panelSize);
 		createComboBoxes();
 		createSliders();
 		setVisible(true);
 	}
-	
+
 	public String getObjectName() {
 		return objectName;
 	}
-	
+
 	public String getMovementName() {
 		return movementName;
 	}
-	
+
 	public int getMovementSpeed() {
 		return movementSpeed;
 	}
-	
+
 	public int getMovementDuration() {
 		return movementDuration;
 	}
-	
+
 	public int getGravityMagnitude() {
 		return gravityMag;
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void createComboBoxes() {
-		// Object Types will define things like how collisions are handled, and input
-		// Will also determine what other options are available for user to define
-		String[] objectTypes = {"Player","Enemy", "Block", "Moving Platform", "Goal", "Scenery"};
+		// Object Types will define things like how collisions are handled, and
+		// input
+		// Will also determine what other options are available for user to
+		// define
+		String[] objectTypes = { "Player", "Enemy", "Block", "Moving Platform",
+				"Goal", "Scenery" };
 		JLabel type = new JLabel("Object Type");
 		JComboBox objectType = new JComboBox(objectTypes);
 		objectType.setPreferredSize(COMBO_SIZE);
-		objectType.addItemListener(new ItemListener(){
+		objectType.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent event) {
 				objectName = event.getItem().toString();
 			}
 		});
-		
-		// Movement type will only be available for Enemy and (in a limited sense)
-		// moving platform.  Aggressive is a maybe, it would be cool to have
-		// some 'smarter' AI that tracks down player (if there is time, of course)
-		String[] movementTypes = {"Pace", "Vertical", "Random", "Jumping", "Aggressive"};
+
+		// Movement type will only be available for Enemy and (in a limited
+		// sense)
+		// moving platform. Aggressive is a maybe, it would be cool to have
+		// some 'smarter' AI that tracks down player (if there is time, of
+		// course)
+		String[] movementTypes = { "Pace", "Vertical", "Random", "Jumping",
+				"Aggressive" };
 		JLabel movement = new JLabel("Movement Pattern");
 		JComboBox movementType = new JComboBox(movementTypes);
 		movementType.setPreferredSize(COMBO_SIZE);
@@ -100,46 +104,48 @@ public class ObjectStatsPanel extends JPanel {
 			public void itemStateChanged(ItemEvent event) {
 				movementName = event.getItem().toString();
 			}
-			
+
 		});
-		
 
 		add(type);
 		add(objectType);
 
 		add(movement);
 		add(movementType);
-		
+
 	}
-	
+
 	private void createSliders() {
-		final JSlider speed = new JSlider(0,10);
+		final JSlider speed = new JSlider(0, 10);
 		JLabel speedLabel = new JLabel("Movement Speed");
 		speed.setLabelTable(speed.createStandardLabels(1, 0));
 		speed.setPaintLabels(true);
+		speed.setValue(0);
 		speed.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent event) {
 				movementSpeed = speed.getValue();
-			}	
+			}
 		});
-		
 
-		final JSlider movementLength = new JSlider(0,10);
+		final JSlider movementLength = new JSlider(0, 10);
 		JLabel movementLabel = new JLabel("Movement Duration");
 		movementLength.setLabelTable(movementLength.createStandardLabels(1, 0));
 		movementLength.setPaintLabels(true);
+		movementLength.setValue(0);
 		movementLength.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent event) {
 				movementDuration = movementLength.getValue();
 			}
 		});
-		
-		final JSlider gravityMagnitude = new JSlider(0,10);
+
+		final JSlider gravityMagnitude = new JSlider(0, 10);
 		JLabel gravityLabel = new JLabel("Gravity Magnitude");
-		gravityMagnitude.setLabelTable(gravityMagnitude.createStandardLabels(1, 0));
+		gravityMagnitude.setLabelTable(gravityMagnitude.createStandardLabels(1,
+				0));
 		gravityMagnitude.setPaintLabels(true);
+		gravityMagnitude.setValue(0);
 		gravityMagnitude.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent event) {
@@ -147,32 +153,26 @@ public class ObjectStatsPanel extends JPanel {
 				myEditor.setGravity(gravityMag);
 			}
 		});
-		
+
 		add(speedLabel);
 		add(speed);
 
-		
 		add(movementLabel);
 		add(movementLength);
-		
+
 		add(gravityLabel);
 		add(gravityMagnitude);
-		
+
 	}
 
-/*import gameEngine.Level;
-
-import javax.swing.JPanel;
-
-public class ObjectStatsPanel extends JPanel {
-	LevelEditor myLevelEdit;
-public ObjectStatsPanel(LevelEditor l){
-	myLevelEdit=l;
-	initialize();
-}
-public void initialize(){
-	makeMainPanel();
-}
-public void makeMainPanel(){}
->>>>>>> 4266c77e617ebdf4cb2558463699dfdb9851edf7*/
+	/*
+	 * import gameEngine.Level;
+	 * 
+	 * import javax.swing.JPanel;
+	 * 
+	 * public class ObjectStatsPanel extends JPanel { LevelEditor myLevelEdit;
+	 * public ObjectStatsPanel(LevelEditor l){ myLevelEdit=l; initialize(); }
+	 * public void initialize(){ makeMainPanel(); } public void
+	 * makeMainPanel(){} >>>>>>> 4266c77e617ebdf4cb2558463699dfdb9851edf7
+	 */
 }
