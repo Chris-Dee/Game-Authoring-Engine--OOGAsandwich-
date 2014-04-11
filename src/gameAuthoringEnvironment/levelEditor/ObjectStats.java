2011b546016f7aa46307a2be27d7df73be94f5be
raswaminathan <rahul.swaminathan@duke.edu>
2014-04-11 18:02:32 -0400

    changed variable names

--- src/gameAuthoringEnvironment/levelEditor/ObjectStats.java
+++ src/gameAuthoringEnvironment/levelEditor/ObjectStats.java
@@ -1,29 +1,40 @@
 package gameAuthoringEnvironment.levelEditor;
 
 public class ObjectStats {
-	//TODO could be int depending on how we do it?
-	public String colType;
-	public Integer collID;
-	public String movementPattern;
-	public Integer speed;
-	public Integer duration;
-	public Integer gravMag;
-	public boolean cameraFollow;
-	public String imageName;
-	//TODO could definitely get rid of this
+	// TODO could be int depending on how we do it?
+	public String myColType;
+	public Integer myCollID;
+	public String myMovementPattern;
+	public Integer mySpeed;
+	public Integer myDuration;
+	public Integer myGravMag;
+	public boolean myCameraFollow;
+	public String myImageName;
+	// TODO could definitely get rid of this
 	public boolean isFloating;
-	public ObjectStats(String c, Integer co, String m, Integer s, Integer d, Integer g, boolean cam, String i, boolean f){
-	colType=c;
-	collID = co;
-	movementPattern=m;
-	speed=s;
-	duration=d;
-	gravMag=g;
-	cameraFollow=cam;
-	imageName=i;
-	isFloating=f;
-}
+
+	public ObjectStats(String colType, Integer collID, String movementPattern, Integer speed, Integer duration,
+			Integer gravMag, boolean cameraFollow, String imageName, boolean floating) {
+		myColType = colType;
+		myCollID = collID;
+		myMovementPattern = movementPattern;
+		mySpeed = speed;
+		myDuration = duration;
+		myGravMag = gravMag;
+		myCameraFollow = cameraFollow;
+		myImageName = imageName;
+		isFloating = floating;
+	}
+
 	public ObjectStats(String[] statsArray) {
-		colType=statsArray[0];
+		myColType = statsArray[0];
+		myCollID = Integer.parseInt(statsArray[1]);
+		myMovementPattern = statsArray[2];
+		mySpeed = Integer.parseInt(statsArray[3]);
+		myDuration = Integer.parseInt(statsArray[4]);
+		myGravMag = Integer.parseInt(statsArray[5]);
+		myCameraFollow = Boolean.parseBoolean(statsArray[6]);
+		myImageName = statsArray[7];
+		isFloating = Boolean.parseBoolean(statsArray[8]);
 	}
 }
