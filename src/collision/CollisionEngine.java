7614236c7fe5545af68d33acfaface12d07c0662
Jeff Hou <j.king.hou@gmail.com>
2014-04-14 00:44:33 -0400

    Added util package for placement of objects.

--- src/collision/CollisionEngine.java
+++ src/collision/CollisionEngine.java
@@ -1,37 +1,38 @@
 package collision;
 
+import util.PositionalType;
 import jgame.JGObject;
 
 public class CollisionEngine {
-	public static CollisionType getCollisionType(JGObject o1, JGObject o2){
+	public static PositionalType getCollisionType(JGObject o1, JGObject o2){
 		if(o1.getBBox().intersects(o2.getBBox())){
 			if(o1.getLastX() + o1.getBBox().width < o2.getLastX()){ // o1 is LEFT of o2
 				if(o1.getLastY() + o1.getBBox().height < o2.getLastY()){ // o1 is TOP of o2
-					return CollisionType.TOPLEFT;
+					return PositionalType.TOPLEFT;
 				} else if(o2.getLastY() + o2.getBBox().height < o1.getLastY()){ // o1 is BOT of o2
-					return CollisionType.BOTLEFT;
+					return PositionalType.BOTLEFT;
 				} else { // o1's height intersects o2's height
-					return CollisionType.LEFT;
+					return PositionalType.LEFT;
 				}
 			} else if(o2.getLastX() + o2.getBBox().width < o1.getLastX()){ // o1 is RIGHT of o2
 				if(o1.getLastY() + o1.getBBox().height < o2.getLastY()){ // o1 is TOP of o2
-					return CollisionType.TOPRIGHT;
+					return PositionalType.TOPRIGHT;
 				} else if(o2.getLastY() + o2.getBBox().height < o1.getLastY()){ // o1 is BOT of o2
-					return CollisionType.BOTRIGHT;
+					return PositionalType.BOTRIGHT;
 				} else { // o1's height intersects o2's height
-					return CollisionType.RIGHT;
+					return PositionalType.RIGHT;
 				}
 			} else { // o1's width intersects o2's width
 				if(o1.getLastY() + o1.getBBox().height < o2.getLastY()){ // o1 is TOP of o2
-					return CollisionType.TOP;
+					return PositionalType.TOP;
 				} else if(o2.getLastY() + o2.getBBox().height < o1.getLastY()){ // o1 is BOT of o2
-					return CollisionType.BOT;
+					return PositionalType.BOT;
 				} else { // o1's height intersects o2's height
-					return CollisionType.CONTINUED;
+					return PositionalType.INTERSECTION;
 				}
 			}
 		}else{
-			return CollisionType.NONE;
+			return PositionalType.NONE;
 		}
 	}
 }
--- src/collision/CollisionType.java
+++
@@ -1,6 +0,0 @@
-package collision;
-
-public enum CollisionType {
-	TOP, LEFT, RIGHT, BOT, TOPLEFT, TOPRIGHT,
-	BOTLEFT, BOTRIGHT, CONTINUED, NONE
-}
