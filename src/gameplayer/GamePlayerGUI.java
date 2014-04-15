f783ae8df4c17d479fd8035095f90724dde54a19
Brett Fox <bsf7@duke.edu>
2014-04-14 21:38:37 -0400

    Option box pops up when play game is selected, need GamePlayer / GameEngine team to help implement with restarting the game

--- src/gameplayer/GamePlayerGUI.java
+++ src/gameplayer/GamePlayerGUI.java
@@ -6,13 +6,21 @@ import javax.swing.JFrame;
 
 import gameEngine.Game;
 
-public class GamePlayerGUI extends JFrame{
-	
-	public GamePlayerGUI(Game loadedGame){
+public class GamePlayerGUI extends JFrame {
+
+	/**
+	 * GUI that runs the actual created game by the user. Shows the JGEngine in
+	 * the main screen with a bar on top with options for the user to use.
+	 * 
+	 * @param loadedGame
+	 * 				Game to be loaded
+	 */
+
+	public GamePlayerGUI(Game loadedGame) {
 		setLayout(new BorderLayout());
 		setJMenuBar(new GamePlayerOptionsBar());
-		add(new GamePlayerGame(loadedGame),BorderLayout.CENTER);
-		
+		add(new GamePlayerGame(loadedGame), BorderLayout.CENTER);
+
 		pack();
 		setVisible(true);
 	}
--- src/gameplayer/GamePlayerOptionsBar.java
+++ src/gameplayer/GamePlayerOptionsBar.java
@@ -6,30 +6,49 @@ import java.awt.event.ActionListener;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-public class GamePlayerOptionsBar extends JMenuBar{
-	
+public class GamePlayerOptionsBar extends JMenuBar {
+
+	private static final String RESTART_GAME_QUESTION_TEXT = "Are you sure you want to start over?";
+	private static final String RESTART_GAME = "Restart game?";
+
 	/**
 	 * Makes JMenuBar at top of the game player.
 	 */
-	public GamePlayerOptionsBar(){
+	public GamePlayerOptionsBar() {
 		initialize();
 	}
-	
-	private void initialize(){
+
+	private void initialize() {
 		JMenu file = new JMenu("File");
 		JMenuItem startItem = new JMenuItem("Play game");
-		startItem.addActionListener(new ActionListener(){
+		startItem.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				int restartGame = JOptionPane.showConfirmDialog(null,
+						RESTART_GAME_QUESTION_TEXT, RESTART_GAME,
+						JOptionPane.YES_NO_OPTION);
+				if (restartGame==0){ //Player chose yes,restart the game
+					System.out.println("yay!");
+				}
+			}
+
+		});
+
+		JMenuItem highScoresItem = new JMenuItem("High Scores");
+		highScoresItem.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 				// TODO Implement action
-				
+
 			}
-			
+
 		});
-		
+
 		file.add(startItem);
-		
+		file.add(highScoresItem);
+
 		add(file);
 		setVisible(true);
 	}
